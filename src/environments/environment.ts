7fc313f477b0f815247b9475f2d95bbda543ebb8
gulatikashish <kgulati94@gmail.com>
2018-02-15 17:43:32 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBLDOu464yZDYBdCXNVl6GUqrXDbJd6j_0',
-    authDomain: 'fir-87614.firebaseapp.com',
-    databaseURL: 'https://fir-87614.firebaseio.com',
-    projectId: 'fir-87614',
-    storageBucket: 'fir-87614.appspot.com',
-    messagingSenderId: '245628880673'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 }
